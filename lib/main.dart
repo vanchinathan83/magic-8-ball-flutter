6dff3165794c5a7f7f82d864095d815327448b51
Vanchinathan <vayipalayamchand@paypal.com>
2020-04-25 22:15:18 -0500

    Add Stateful widgets and randomisation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('AMA'),
+            backgroundColor: Colors.blue,
+          ),
+          body: AMA(),
+        ),
       ),
     );
+
+class AMA extends StatefulWidget {
+  @override
+  _AMAState createState() => _AMAState();
+}
+
+class _AMAState extends State<AMA> {
+  int randomAnswer = Random().nextInt(5) + 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    randomAnswer = Random().nextInt(5) + 1;
+                  });
+                },
+                child: Image.asset('images/ball$randomAnswer.png'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
